81cb30b3f78b1bf3e95682275003e4741b83e6e7
thenumbersguya <waterwatch818@gmail.com>
2026-05-02 20:21:17 -0400

    free sing functions

--- script/singfunc.js
+++ script/singfunc.js
@@ -74,7 +74,7 @@ class SingularityFunction {
             ||(EN(getSingLevel()).add(game.incrementyverse==0?game.manifolds-game.sing.m:0).minus(game.incrementyverse==1?game.spentENFunctions:game.spentFunctions).gte(this.price))
             && !(game.sfBought.indexOf(this.id) > -1)
             && (this.prereq.some(id => {return game.sfBought.includes(id)}) || this.id==81)
-            && game.omegaChallenge!=6
+            && game.omegaChallenge!=78
             && game.sfBought.indexOf(this.splitpath[0]) == -1
             && game.sfBought.indexOf(this.splitpath[1]) == -1
             && this.specialReq()
@@ -85,36 +85,36 @@ class SingularityFunction {
 let SF = (a,b,c,d) => new SingularityFunction(a,b,c,d)
 let singfunctions = [
   SF(0, [], 11, [0,0]),
-  SF(24, [11], 21, [0,0]),
-  SF(15, [11], 22, [0,0]),
-  SF(20, [11], 23, [0,0]),
-  SF(6, [22], 31, [0,0]),
-  SF(10, [22], 32, [0,0]),
+  SF(0, [11], 21, [0,0]),
+  SF(0, [11], 22, [0,0]),
+  SF(0, [11], 23, [0,0]),
+  SF(0, [22], 31, [0,0]),
+  SF(0, [22], 32, [0,0]),
   SF(0, [31], 41, [0,0]),
-  SF(2, [31,32], 42, [0,0]),
+  SF(0, [31,32], 42, [0,0]),
   SF(0, [42], 51, [0,0]),
-  SF(12, [42], 52, [0,0]), //uhh, sniped... lol we both got sniped by each other
-  SF(17, [51], 61, [0,0]),
-  SF(15, [51], 62, [0,0]),
-  SF(42, [51], 63, [0,0]),
+  SF(0, [42], 52, [0,0]), //uhh, sniped... lol we both got sniped by each other
+  SF(0, [51], 61, [0,0]),
+  SF(0, [51], 62, [0,0]),
+  SF(0, [51], 63, [0,0]),
   SF(0, [61], 71, [0,0]),
-  SF(16, [62], 72, [0,0]),
+  SF(0, [62], 72, [0,0]),
   SF(0, [63], 73, [0,0]),
-  SF(EN(1000), [71,72,73], 81, [0,0]),
-  SF(EN(1500), [81], 91, [0,0]),
-  SF(EN(2500), [81], 92, [0,0]),
-  SF(EN(1e6), [81], 93, [0,0]),
+  SF(EN(0), [71,72,73], 81, [0,0]),
+  SF(EN(0), [81], 91, [0,0]),
+  SF(EN(0), [81], 92, [0,0]),
+  SF(EN(0), [81], 93, [0,0]),
   SF(EN(0), [91], 101, [0,0]),
-  SF(EN(5000), [92], 102, [0,0]),
-  SF(EN("ee6"), [93], 103, [0,0]),
+  SF(EN(0), [92], 102, [0,0]),
+  SF(EN("0"), [93], 103, [0,0]),
   SF(EN(0), [101], 111, [0,0]),
-  SF(EN(40000), [102], 112, [0,0]),
-  SF(EN(125), [101,103], 113, [0,0]),
-  SF(EN(2e5), [102], 114, [0,0]),
-  SF(EN("ee8"), [103], 115, [0,0]),
+  SF(EN(0), [102], 112, [0,0]),
+  SF(EN(0), [101,103], 113, [0,0]),
+  SF(EN(0), [102], 114, [0,0]),
+  SF(EN("0"), [103], 115, [0,0]),
   SF(EN(0), [115], 121, [0,0]),
-  SF(EN("10^^100"), [121], 131, [0,0]),
-  SF(EN("10^^^^100"), [121], 132, [0,0]),
+  SF(EN("0"), [121], 131, [0,0]),
+  SF(EN("0"), [121], 132, [0,0]),
   SF(EN(0), [131], 141, [0,0]),
 ];
 
